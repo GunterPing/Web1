363104e5a51416bfd4a0cfcdb9c95e56bd9a2fa0
Gunter <73429064+GunterPing@users.noreply.github.com>
2021-08-08 18:27:29 +0200

    Update settings.py

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s!mzc2rjzcagwr(=v&1ljv##uv9sws-x6@1%i4r8dh29vo%1n6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
